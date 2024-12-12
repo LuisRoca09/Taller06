ebfc46e6ed3a50421322252a493ecdb10d089002
Luis Roca <leroca@espol.edu.ec>
2024-12-11 23:43:43 -0500

    A

--- Empleado.java
+++ Empleado.java
@@ -1,46 +1,19 @@
-public class Empleado {
+public abstract class Empleado {
     private String nombre;
     private double salarioBase;
     private int horasTrabajadas;
     private String departamento;
-    private double tarifaHora;
 
-    public Empleado(){}
-    public Empleado(String nombre, double salarioBase, int horasTrabajadas, double tarifaHora, String departamento) {
+    public Empleado(String nombre, double salarioBase, int horasTrabajadas, String departamento) {
         this.nombre = nombre;
         this.salarioBase = salarioBase;
         this.horasTrabajadas = horasTrabajadas;
-        this.tarifaHora = tarifaHora;
         this.departamento = departamento;
     }
 
-    public double calcularSalario() {
-        double salarioTotal = salarioBase;
-        if (salarioBase>0) {
-            if (horasTrabajadas >= 0) {
-                // Horas trabajadas normales = 40;
-                if (horasTrabajadas > 40) {
-                    salarioTotal += (horasTrabajadas - 40) * 50; // Pago de horas extra
-                }
-            }else {
-                throw new IllegalArgumentException("Las horas trabajadas deben ser mayor o igual a 0");
-            }
-        } else {
-            throw new IllegalArgumentException("El salario debe ser mayor o igual a 0");
-        }
-        switch (departamento) {
-            case "Sistemas":
-                salarioTotal += 20;
-                break;
-            case "Contabilidad":
-                salarioTotal += 10;
-                break;
-            default:
-                break;
-        }
-        return salarioTotal;
-    }
+    public abstract double calcularSalario();
 
+    // Getters y setters
     public String getNombre() {
         return nombre;
     }
@@ -65,14 +38,6 @@ public class Empleado {
         this.horasTrabajadas = horasTrabajadas;
     }
 
-    public double getTarifaHora() {
-        return tarifaHora;
-    }
-
-    public void setTarifaHora(double tarifaHora) {
-        this.tarifaHora = tarifaHora;
-    }
-
     public String getDepartamento() {
         return departamento;
     }
@@ -80,6 +45,4 @@ public class Empleado {
     public void setDepartamento(String departamento) {
         this.departamento = departamento;
     }
-
-    // Más metodos
 }
